1c75fa1cbafb8ea00add9c13b49e20319ae3afdc
chaman-k <chamansullia@gmail.com>
2019-04-25 07:58:42 +0530

    init

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+const mongoose = require('mongoose');
+var router = express.Router();
+const fs = require('fs'); 
+//const csv = require('csv-parser');
+const csv=require("csvtojson");
+mongoose.connect("mongodb+srv://dbuser:test123@cluster0-bkhg1.gcp.mongodb.net/test?retryWrites=true", {
+    useNewUrlParser: true
+});
+
+var tSchema = new mongoose.Schema({
+    threat_level: String,
+    accuracy: Number,
+    time: String,
+    filename: String
+});
+
+var Pred = mongoose.model("Pred", tSchema);
+
+const inputFilePath = '/home/chaman/projects/fyp/segment/logs/segmentation.csv';
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  const jsonArray=await csv().fromFile(inputFilePath);
+  console.log(jsonArray);
+  const files1 = await Pred.find({}).exec();
+  //  console.log(files1);
+  const infiltration = await Pred.find({threat_level: 'infiltration'}).exec();
+  console.log(infiltration);
+  
+  const blast = await Pred.find({threat_level: 'blast'}).exec();
+  const normal = await Pred.find({threat_level: 'normal'}).exec();
+  const war = await Pred.find({threat_level: 'war'}).exec();
+
+  res.render('index', { files: jsonArray, files1: files1, infiltration: infiltration, blast: blast, normal: normal, war: war });
+});
+
+module.exports = router;
